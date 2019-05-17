d34387a66896ca770cbb76fce3756cfbf0194962
Micah Lee <micah@micahflee.com>
2019-05-16 18:48:29 -0700

    Center the sidebar, and make numbers in statistics have comma separators

--- semiphemeral/static/js/base.js
+++ semiphemeral/static/js/base.js
@@ -1,14 +1,18 @@
+function comma_formatted(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 $(function(){
   $.get('/api/statistics', function(data){
     is_configured = (data.is_configured ? 'yes' : 'no');
     $('.statistics .is_configured').text(is_configured);
     $('.statistics .last_fetch').text(data.last_fetch);
-    $('.statistics .my_tweets').text(data.my_tweets);
-    $('.statistics .my_retweets').text(data.my_retweets);
-    $('.statistics .deleted_tweets').text(data.deleted_tweets);
-    $('.statistics .deleted_retweets').text(data.deleted_retweets);
-    $('.statistics .excluded_tweets').text(data.excluded_tweets);
-    $('.statistics .other_tweets').text(data.other_tweets);
-    $('.statistics .threads').text(data.threads);
+    $('.statistics .my_tweets').text(comma_formatted(data.my_tweets));
+    $('.statistics .my_retweets').text(comma_formatted(data.my_retweets));
+    $('.statistics .deleted_tweets').text(comma_formatted(data.deleted_tweets));
+    $('.statistics .deleted_retweets').text(comma_formatted(data.deleted_retweets));
+    $('.statistics .excluded_tweets').text(comma_formatted(data.excluded_tweets));
+    $('.statistics .other_tweets').text(comma_formatted(data.other_tweets));
+    $('.statistics .threads').text(comma_formatted(data.threads));
   })
 })
