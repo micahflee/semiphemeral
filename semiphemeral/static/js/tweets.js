5292d7e5e5d198d949f2bb799a919bbd6f790485
Micah Lee <micah@micahflee.com>
2019-05-23 21:44:52 -0700

    Start implementing pagination

--- semiphemeral/static/js/tweets.js
+++ semiphemeral/static/js/tweets.js
@@ -1,25 +1,78 @@
+function display_tweets(ids, page=0, count=100) {
+  console.log('display_tweets', 'ids=' + ids.length + ', page='+page+', count='+count);
+
+  // Display info
+  $('.info').append($('<div/>').html('Page '+comma_formatted(page)+' of '+comma_formatted(ids.length-1)+' tweets are staged for deletion'));
+
+  $('.tweets-to-delete').empty();
+  $('.pagination').empty();
+  num_pages = Math.ceil(ids.length / count);
+
+  function add_item(text, action=null) {
+    console.log('add_item', text)
+    var $item = $('<span/>').addClass('item').text(text);
+    if(action) {
+      $item.click(action);
+    } else {
+      $item.addClass('item-current');
+    }
+    $('.pagination').append($item);
+
+  }
+
+  // Pagination controls
+  if(page > 0) {
+    add_item('Previous', function(){
+      display_tweets(ids, page-1, count);
+    });
+  }
+
+  for(var i=page-10; i<page; i++) {
+    if(i >= 0) {
+      add_item(i, function(){
+        display_tweets(ids, i, count);
+      });
+    }
+  }
+
+  add_item(page);
+
+  for(var i=page+1; i<page+10; i++) {
+    add_item(i, function(){});
+  }
+
+  if(page < num_pages-1) {
+    add_item('Next', function(){
+      display_tweets(ids, page+1, count);
+    });
+  }
+
+  // Display the page of tweets
+  for(var i=page*count; i<(page+1)*count; i++) {
+    $('.tweets-to-delete').append('<div class="tweet" id="tweet-'+ids[i]+'"></div>');
+    twttr.widgets.createTweet(ids[i], $('#tweet-'+ids[i])[0], {
+      'dnt': true
+    });
+  }
+}
+
 $(function(){
-  $('.tweets-to-delete').html('loading please wait...');
+  // Ajax loader
+  $('.tweets-to-delete').html('<img src="/static/img/loading.gif" alt="">');
 
   // Load all tweets to delete
   $.get('/api/tweets-to-delete', function(tweets){
-    $('.tweets-to-delete').empty();
-
-    // Make a list of tweet ids
     var ids = [];
     for(var id in tweets) {
       ids.push(id);
     }
 
-    count = 0;
-    for(var id in tweets) {
-      $('.tweets-to-delete').append('<div class="tweet" id="tweet-'+id+'"></div>');
-      twttr.widgets.createTweet(id, $('#tweet-'+id)[0], {
-        'dnt': true
-      });
-
-      count++;
-      if(count == 20) { break; }
+    window.semiphemeral = {
+      tweets: tweets,
+      ids: ids
     }
+
+    // Display tweets
+    display_tweets(self.semiphemeral.ids);
   })
 })
